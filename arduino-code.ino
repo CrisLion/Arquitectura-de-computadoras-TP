d9ffd2af5ac71543b7a2065e180a3a98fe1ef089
Cristian Quito <cristianandresquito@gmail.com>
2023-05-04 14:45:22 -0500

    feat: option key 1 added

--- arduino-code.ino
+++ arduino-code.ino
@@ -1,5 +1,7 @@
 #include <Keypad.h>
 #include <LiquidCrystal_I2C.h>
+
+#pragma region GLOBAL_VAR
 //leds
 int led1=13;
 
@@ -11,7 +13,7 @@ int ScaledAnalogPinPir2=0;
 
 //piezo
 int piezo=12;
-bool valorTeclado=false;
+int is_Piezo_On = false;
 
 //sensor Sonido
 int pinDesencadenador=2; //trig emite onda de sonido
@@ -22,6 +24,7 @@ char codigo[4];
 int opc=0;
 int cont=0;
 
+int IS_SYSTEM_ON = false; 
 
 //---Teclado
 const byte ROWS = 4; //filas
@@ -42,6 +45,7 @@ byte i=0;
 //porcion de memoria, cantidad de caracteres, cantidad de lineas
 LiquidCrystal_I2C lcd(34, 16, 2);
 
+#pragma endregion GLOBAL_VAR
 
 void setup()
 {
@@ -62,19 +66,29 @@ void setup()
 	for( i=0;i<8;i++){
 		pinMode(i,OUTPUT);
 	}
+
+	keypad.addEventListener(Key_instructions);
+	menu();
 }
 
 void loop()
 {
 
-	//menu();
-	
-	//pir1 and pir2
-	Pirs_Working();
-	
+	char key = keypad.getKey();
 
-	//sensor sonido
-	Ultrasound_Working();
+	if (key) {
+		Serial.println(key);
+	}
+	
+	if (IS_SYSTEM_ON){
+		//pir1 and pir2
+		Pirs_Working();
+		//sensor sonido
+		Ultrasound_Working();
+		//Piezo loud
+		Piezo_Working();
+	}
+	
   
 	//tecla presionada
 	// char pulsacion=keypad.getKey();
@@ -151,7 +165,7 @@ void Ultrasound_Working(){
 
 	if(distancia<130){//120cm
 		digitalWrite(led1,HIGH);
-		tone(piezo, 523,200);
+		is_Piezo_On = true;
 		Serial.println("sonido Detectado");
 	}
 	else{
@@ -171,10 +185,28 @@ void Pirs_Working(){
 		Serial.println("Movimiento Detectado");
 		Serial.println(ScaledAnalogPinPir1);
 		digitalWrite(led1, HIGH);
-		tone(piezo, 523,200);
+		is_Piezo_On = true;
 	}
 	else{
 		digitalWrite(led1, LOW);
 		noTone(piezo);
 	}
+}
+
+void Piezo_Working(){
+	if (is_Piezo_On)
+		tone(piezo, 523,200);
+}
+
+//Key_instructions: That means that pressing a key will execute a function or change a variable value
+void Key_instructions(char key){
+	switch (keypad.getState()){
+    case PRESSED:
+		
+		switch (key){
+
+			case '1': IS_SYSTEM_ON = true ; break;
+
+		}
+  	}
 }
